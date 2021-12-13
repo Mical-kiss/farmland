7a3b4d149d5a45a1f849685c4f03447cc53509bd
zhengwj <516114697@qq.com>
2021-12-13 23:49:27 +0800

    test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="这里应该种点什么东西的"/>
   </div>
 </template>
 
